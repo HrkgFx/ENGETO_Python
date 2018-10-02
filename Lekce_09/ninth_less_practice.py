1a0d077f34bfcb9e2572ac80b39179d09140afcf
Rob <jancarik.r@gmail.com>
2018-10-02 19:23:05 +0200

    funkcni try

--- Lekce_09/ninth_less_practice.py
+++ Lekce_09/ninth_less_practice.py
@@ -9,14 +9,14 @@
 # if :
 #   x+1
 
-# print ('Ahoj')
-# if 1 < 2:
-#     print(3)
-#     # print(1/'1')
+print ('Ahoj')
+if 1 < 2:
+    print(3)
+    # print(1/'1')
 
 vstup = input ('Zadej cislo: ')
 #zkus vykonat kod co mas u sebe
-    try:
-        cislo = int(vstup)
-    except:
-        print('Nezadal jsi cislo.')
+try:
+    cislo = int(vstup)
+except:
+    print('Nezadal jsi cislo.')
