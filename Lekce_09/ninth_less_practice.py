7be409988f22da1834496b2c485dc4163e025b43
Rob <jancarik.r@gmail.com>
2018-10-02 19:22:23 +0200

    nefunkcni try

--- Lekce_09/ninth_less_practice.py
+++ Lekce_09/ninth_less_practice.py
@@ -1,17 +1,22 @@
 # cislo = int('abc')
 # if 1 < 2:
-while 1 < 2:
-    print(1)
-    print(3)
-     # print({1:1}[2])
-    print(1/'1')
+# while 1 < 2:
+#     print(1)
+#     print(3)
+#      # print({1:1}[2])
+#     print(1/'1')
 
 # if :
 #   x+1
 
-print ('Ahoj')
-while 1 < 2:
-    print(1)
-    print(3)
-     # print({1:1}[2])
-    print(1/'1')
+# print ('Ahoj')
+# if 1 < 2:
+#     print(3)
+#     # print(1/'1')
+
+vstup = input ('Zadej cislo: ')
+#zkus vykonat kod co mas u sebe
+    try:
+        cislo = int(vstup)
+    except:
+        print('Nezadal jsi cislo.')
