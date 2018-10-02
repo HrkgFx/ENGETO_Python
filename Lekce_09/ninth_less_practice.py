7c1d4d76c86e78d600109ebb02b24f85f7174fd6
Rob <jancarik.r@gmail.com>
2018-10-02 19:52:26 +0200

    except, finally atd.

--- Lekce_09/ninth_less_practice.py
+++ Lekce_09/ninth_less_practice.py
@@ -14,12 +14,27 @@ if 1 < 2:
     print(3)
     # print(1/'1')
 
-vstup = input ('Zadej cislo: ')
-#zkus vykonat kod co mas u sebe
-try:
-    cislo = int(vstup) # kde vznikla chyba rovno skace na except
-    vysledek = 100 / cislo    
-except ValueError:
-    print('Nezadal jsi cislo.')
-except ZeroDivisionError:
-    print('Nelze delit nulou.')
+
+while 1:
+    vstup = input ('Zadej cislo: ')
+    #zkus vykonat kod co mas u sebe
+    try:
+        cislo = int(vstup) # kde vznikla chyba rovno skace na except
+        vysledek = 100 / cislo
+    # except ValueError:
+    #     print('Nezadal jsi cislo.')
+    # except ZeroDivisionError:
+    #     print('Nelze delit nulou.')
+
+    except (ValueError, ZeroDivisionError): #je to TUPLE
+        print('Blbost')
+
+    else: # kdyz se provede try v pohode tak proved toto
+        print('Výsledek je: ', vysledek)
+        break
+
+nasobek = cislo * 37
+print(nasobek)
+
+# finally:
+#     print('Děkujeme za pouzivani aplikace.')
