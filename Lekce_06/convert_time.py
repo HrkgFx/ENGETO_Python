13b59e52ac0d9e62a791b075d99d4b8a4de9ae20
Rob <jancarik.r@gmail.com>
2018-10-14 22:15:13 +0200

    con time

---
+++ Lekce_06/convert_time.py
@@ -0,0 +1,43 @@
+# Please enter your time: 17 : 35
+# Converted to English: 5 : 35 PM
+
+# while True:
+#     time = input('Please enter your time: ')
+#     hours, mins = time.split(':')
+#     hours = int(hours)
+#
+#     if hours >= 12:
+#         eng_hours = hours - 12
+#         print('Converted time is: {}:{} PM'.format(eng_hours,mins))
+#     else:
+#         print('Converted time is: {}:{} AM'.format(hours,mins))
+
+
+# time = input('Please enter your time: ')
+#
+# hours, mins = time.split(':')
+#
+# # variable_name = expression1 if condition else expression2
+# adjusted_h = hours if int(hours)==12 else str(int(hours) % 12)
+#
+# daytime = ['AM','PM'][int(hours)>=12]
+#
+# print('Converted to English: ' + adjusted_h + ':' + mins + ' ' + daytime)
+
+
+# hours = 6
+#
+# daytime = ['AM','PM'][int(hours)>=12]
+# print(bool(dayt))
+# print(daytime)
+
+
+my_str = 'Python'
+result = ''
+
+if my_str.istitle():
+    result = 'Titlecased'
+else:
+    result = 'Not titlecased'
+
+print(result)
